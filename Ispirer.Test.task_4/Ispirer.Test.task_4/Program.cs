483081c2de67d8c7bc3bf560d4dceb62b8dfcdcd
Unknown <annanova913@gmail.com>
2018-02-02 00:27:46 +0300

    последниие 2(1-9)
    должны работать)

---
+++ Ispirer.Test.task_4/Ispirer.Test.task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispirer.Test.task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = args.Length ==  0
+                ? Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)
+                : args[0];
+            DirectoryInfo di_info = new DirectoryInfo(path);
+            scan_folder(di_info, "");
+        }
+
+        public static void scan_folder(DirectoryInfo info, string spaces)
+        {
+            Console.WriteLine(spaces+info.Name);
+            DirectoryInfo[] childrens = info.GetDirectories();
+            foreach (var directoryInfo in childrens)
+            {
+                scan_folder(directoryInfo, spaces + '\t');
+
+            }
+
+        }
+    }
+}
---
+++ Ispirer.Test.task_8/Ispirer.Test.task_8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispirer.Test.task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            var assembly = Assembly.LoadFile(path);
+
+            foreach (var group in assembly.GetTypes().GroupBy(i=>i.Namespace))
+            {
+                Console.WriteLine("namespase " + group.First().Namespace);
+
+                foreach (var type_g in group)
+                {
+                    Console.WriteLine(type_g.FullName);
+
+                }
+
+            }
+
+        }
+    }
+}
